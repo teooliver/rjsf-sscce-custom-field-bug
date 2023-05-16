f15468c0598463d7886bc926e0c6728700e3ddaf
Teo Oliver <teooliver@hollandandbarrett.com>
2023-05-16 09:43:41 +0100

    feat: create sscce

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import validator from '@rjsf/validator-ajv8';
+import Form from '@rjsf/bootstrap-4';
+import { RJSFSchema, UiSchema, FieldProps, RegistryFieldsType } from '@rjsf/utils';
+import { nanoid } from "nanoid";
 import './App.css';
 
+
+const IdGenerator = ({ formData, schema: { title, description }, name, onChange }: FieldProps) => {
+  const [val, setVal] = useState<string>("");
+
+  useEffect(() => {
+    if (formData) {
+      setVal(formData);
+    } else {
+      const id = nanoid();
+      setVal(id);
+      // If you remove this onChange call we can add items the array, 
+      // otherwise it flickers a new item and removes it.
+      onChange(id); 
+    }
+  }, [formData, onChange]);
+
+  return (
+    <div>
+      <label htmlFor="id">ID:</label>
+      <input type="text" id="id" name="id" value={val} disabled />
+    </div>
+  );
+};
+
+
+const schema: RJSFSchema = {
+  "type": "object",
+  "properties": {
+    "categories": {
+      "type": "array",
+      "title": "Categories",
+      "items": {
+        "type": "object",
+        "properties": {
+          "id": {
+            "title": "Id",
+            "type": "string"
+          },
+          "text": {
+            "type": "string",
+            "title": "Text"
+          },
+        }
+      }
+    }
+  }
+}
+const uiSchema: UiSchema = {
+  "categories": {
+    "items": {
+      "id": {
+        "ui:field": "idGenerator"
+      }
+    }
+  }
+};
+
+const fields: RegistryFieldsType = { idGenerator: IdGenerator };
+
+
 function App() {
+  const log = (type: any) => console.log.bind(console, type);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Custom field `onChange` breaks "array" type in an schema
+      </p>
+      <div className="form">
+        <Form
+          schema={schema}
+          uiSchema={uiSchema}
+          validator={validator}
+          fields={fields}
+          onChange={log('changed')}
+          onSubmit={log('submitted')}
+          onError={log('errors')}
+        />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
